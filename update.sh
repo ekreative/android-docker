61ffa9b8d157d4c84ebc9b3dafe4adfd471474db
Fred Cox <mcfedr@gmail.com>
2021-03-15 15:39:13 +0200

    Add node14 variant

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 set -e
 
 variants=('29' '30')
+node_variants=('12' '14')
+default_node_variant='12'
 
 declare -A buildTools=(
   ['29']='29.0.3'
@@ -14,49 +16,60 @@ declare -A extraPackages=(
 )
 
 for variant in "${variants[@]}"; do
-  for type in 'default' 'emulator' 'ndk' 'stf-client' 'jdk14'; do
-    template="Dockerfile.template"
-    if [ "$type" != "default" ]; then
-      dir="$variant-$type"
-    else
-      dir="$variant"
-    fi
-    rm -rf "$dir"
-    mkdir -p "$dir"
-    cp -r tools/ "$dir/tools/"
+  for node_variant in "${node_variants[@]}"; do
+    for type in 'default' 'emulator' 'ndk' 'stf-client' 'jdk14'; do
+      template="Dockerfile.template"
+      if [ "$type" != "default" ]; then
+        dir="$variant-$type"
+        if [ "$node_variant" != "$default_node_variant" ]; then
+          break 2
+        fi
+      else
+        dir="$variant"
+      fi
 
-    extraSed=''
-    if [ "$type" = "emulator" ]; then
-      cp -r tools-emulator/ "$dir/tools-emulator/"
-    else
-      extraSed='
+      if [ "$node_variant" != "$default_node_variant" ]; then
+        dir="$dir-node$node_variant"
+      fi
+
+      rm -rf "$dir"
+      mkdir -p "$dir"
+      cp -r tools/ "$dir/tools/"
+
+      extraSed=''
+      if [ "$type" = "emulator" ]; then
+        cp -r tools-emulator/ "$dir/tools-emulator/"
+      else
+        extraSed='
+          '"$extraSed"'
+          /##<emulator>##/,/##<\/emulator>##/d;
+        '
+      fi
+      if [ "$type" != "ndk" ]; then
+        extraSed='
+          '"$extraSed"'
+          /##<ndk>##/,/##<\/ndk>##/d;
+        '
+      fi
+      if [ "$type" != "stf-client" ]; then
+        extraSed='
+          '"$extraSed"'
+          /##<stf-client>##/,/##<\/stf-client>##/d;
+        '
+      fi
+      if [ "$type" = "jdk14" ]; then
+        jdkVersion="14"
+      else
+        jdkVersion="8"
+      fi
+      sed -E '
         '"$extraSed"'
-        /##<emulator>##/,/##<\/emulator>##/d;
-      '
-    fi
-    if [ "$type" != "ndk" ]; then
-      extraSed='
-        '"$extraSed"'
-        /##<ndk>##/,/##<\/ndk>##/d;
-      '
-    fi
-    if [ "$type" != "stf-client" ]; then
-      extraSed='
-        '"$extraSed"'
-        /##<stf-client>##/,/##<\/stf-client>##/d;
-      '
-    fi
-    if [ "$type" = "jdk14" ]; then
-      jdkVersion="14"
-    else
-      jdkVersion="8"
-    fi
-    sed -E '
-      '"$extraSed"'
-      s/%%VARIANT%%/'"$variant"'/;
-      s/%%BUILD_TOOLS%%/'"${buildTools[$variant]}"'/;
-      s/%%EXTRA_PACKAGES%%/'"${extraPackages[$variant]}"'/;
-	  s/%%JDK_VERSION%%/'"$jdkVersion"'/;
-    ' $template >"$dir/Dockerfile"
+        s/%%VARIANT%%/'"$variant"'/;
+        s/%%NODE_VARIANT%%/'"$node_variant"'/;
+        s/%%BUILD_TOOLS%%/'"${buildTools[$variant]}"'/;
+        s/%%EXTRA_PACKAGES%%/'"${extraPackages[$variant]}"'/;
+      s/%%JDK_VERSION%%/'"$jdkVersion"'/;
+      ' $template >"$dir/Dockerfile"
+    done
   done
 done
